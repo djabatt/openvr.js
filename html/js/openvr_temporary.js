048e51881b11a7da39748ffd6a93a3668bcf2fa6
Tylor <tylorlouis@ucla.edu>
2014-07-15 12:28:27 -0700

    Fully re-install pointerlock controls (temporary). Start experimenting with css to json parser

--- html/js/openvr_temporary.js
+++ html/js/openvr_temporary.js
@@ -29,6 +29,11 @@
             }
         };
 
+
+    $("#ovr-style").parsecss(function( result ) {
+        console.log(result);
+    });
+
     // Helper function
 
     function toRad( angle ) {
@@ -53,6 +58,58 @@
             params.position.z
         );
         return light;
+    }
+
+
+    var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
+
+    if ( havePointerLock ) {
+     var element = document.body;
+
+     var fullscreenchange = function ( event ) {
+         if (document.fullscreenElement === element ||
+                 document.mozFullscreenElement === element ||
+                 document.mozFullScreenElement === element) {
+             document.removeEventListener( 'fullscreenchange', fullscreenchange );
+             document.removeEventListener( 'mozfullscreenchange', fullscreenchange );
+             element.requestPointerLock();
+         }
+     }
+
+     document.addEventListener( 'fullscreenchange', fullscreenchange, false );
+     document.addEventListener( 'mozfullscreenchange', fullscreenchange, false );
+
+     element.requestFullscreen = element.requestFullscreen || element.mozRequestFullscreen || element.mozRequestFullScreen || element.webkitRequestFullscreen;
+
+     var pointerlockchange = function ( event ) {
+         if (document.pointerLockElement === element ||
+                 document.mozPointerLockElement === element ||
+                 document.webkitPointerLockElement === element) {
+             controls.enabled = true;
+         } else {
+             controls.enabled = false;
+         }
+     }
+
+     var pointerlockerror = function ( event ) {
+     }
+
+     // Hook pointer lock state change events
+     document.addEventListener( 'pointerlockchange', pointerlockchange, false );
+     document.addEventListener( 'mozpointerlockchange', pointerlockchange, false );
+     document.addEventListener( 'webkitpointerlockchange', pointerlockchange, false );
+
+     document.addEventListener( 'pointerlockerror', pointerlockerror, false );
+     document.addEventListener( 'mozpointerlockerror', pointerlockerror, false );
+     document.addEventListener( 'webkitpointerlockerror', pointerlockerror, false );
+
+     document.body.addEventListener( 'click', function ( event ) {
+         // Ask the browser to lock the pointer
+         element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
+         element.requestPointerLock();
+     }, false );
+    } else {
+     instructions.innerHTML = 'Your browser doesn\'t seem to support Pointer Lock API';
     }
 
     function initObjects() {
