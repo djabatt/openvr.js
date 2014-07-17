b11a55f06de471427cf52e4553502302b1ac20cb
Tylor <tylorlouis@ucla.edu>
2014-07-17 12:37:05 -0700

    Reinstall camera after loading from css, drop some json loading code from init function

--- html/js/openvr_temporary.js
+++ html/js/openvr_temporary.js
@@ -117,8 +117,6 @@
     function initObjects() {
         // Clear objects and moving objects list
         objects = [];
-        // Init camera
-        camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
 
         // Init scene
         scene = new THREE.Scene();
@@ -138,150 +136,30 @@
         if ( SCENE.scene.fog )
             scene.fog = new THREE.Fog( 0xffffff, 0, 750 );
 
-        // Init controls
-        // controls = new THREE.OculusRiftControls( camera );
-        controls = new THREE.PointerLockControls( camera );
-        scene.add( controls.getObject() );
-
         // Light ray caster
         ray = new THREE.Raycaster();
         ray.ray.direction.set( 0, -1, 0 );
 
-        // Floor - aka the plane (width, height, widthSegments, heightSegments )
-        geometry = new THREE.PlaneGeometry( SCENE.floor.width, SCENE.floor.height, 1, 1 );
-        geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
+        // // Floor - aka the plane (width, height, widthSegments, heightSegments )
+        // geometry = new THREE.PlaneGeometry( SCENE.floor.width, SCENE.floor.height, 1, 1 );
+        // geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
 
-        for ( var i = 0, l = geometry.faces.length; i < l; i ++ ) {
-            var face = geometry.faces[ i ];
-            var hexcolor = parseInt( "0x" + SCENE.floor.color );
-            face.vertexColors[ 0 ] = new THREE.Color().setHex( hexcolor );
-            face.vertexColors[ 1 ] = new THREE.Color().setHex( hexcolor );
-            face.vertexColors[ 2 ] = new THREE.Color().setHex( hexcolor );
-            face.vertexColors[ 3 ] = new THREE.Color().setHex( hexcolor );
-        }
+        // for ( var i = 0, l = geometry.faces.length; i < l; i ++ ) {
+        //     var face = geometry.faces[ i ];
+        //     var hexcolor = parseInt( "0x" + SCENE.floor.color );
+        //     face.vertexColors[ 0 ] = new THREE.Color().setHex( hexcolor );
+        //     face.vertexColors[ 1 ] = new THREE.Color().setHex( hexcolor );
+        //     face.vertexColors[ 2 ] = new THREE.Color().setHex( hexcolor );
+        //     face.vertexColors[ 3 ] = new THREE.Color().setHex( hexcolor );
+        // }
 
-        // Compile floor
-        material = new THREE.MeshBasicMaterial( { vertexColors: THREE.VertexColors } );
-        mesh = new THREE.Mesh( geometry, material );
+        // // Compile floor
+        // material = new THREE.MeshBasicMaterial( { vertexColors: THREE.VertexColors } );
+        // mesh = new THREE.Mesh( geometry, material );
 
-        // Add floor to scene
-        scene.add( mesh );
+        // // Add floor to scene
+        // scene.add( mesh );
 
-        // Objects
-        SCENE.objects.forEach( function( object ) {
-            // Default values
-            object.scale = object.scale ? parseInt(object.scale) : 20;
-            object.color = object.color || "ffffff"
-            object.colors = object.colors || [];
-            if (object.colors.length < 3) {
-                for (var i = 0; i < 3; i ++) {
-                    object.colors[ i ] = object.colors[ i ] || parseInt("ffffff");
-                }
-            }
-            if (!object.position)
-                object.position = {};
-            object.position.x = object.position.x || 0;
-            object.position.y = object.position.y || 0;
-            object.position.z = object.position.z || 0;
-
-            switch (object.type) {
-                case "cube":
-                    geometry = new THREE.BoxGeometry( object.scale, object.scale, object.scale );
-
-                    for ( var i = 0, l = geometry.faces.length; i < l; i ++ ) {
-
-                        var face = geometry.faces[ i ];
-                        face.vertexColors[ 0 ] = new THREE.Color().setHex( parseInt( "0x" + object.colors[ 0 ] ) );
-                        face.vertexColors[ 1 ] = new THREE.Color().setHex( parseInt( "0x" + object.colors[ 1 ] ) );
-                        face.vertexColors[ 2 ] = new THREE.Color().setHex( parseInt( "0x" + object.colors[ 2 ] ) );
-                        face.vertexColors[ 3 ] = new THREE.Color().setHex( parseInt( "0x" + object.colors[ 3 ] ) );
-
-                        material = new THREE.MeshPhongMaterial( { specular: 0xffffff, shading: THREE.FlatShading,
-                            vertexColors: THREE.VertexColors } );
-                    }
-                    break;
-                case "sphere":
-                    geometry = new THREE.SphereGeometry( object.scale, 32 );
-                    material = new THREE.MeshLambertMaterial( { color: parseInt("0x" + object.color ) } );
-                    break;
-                case "cylinder":
-                    geometry = new THREE.CylinderGeometry( object.topr, object.botr, object.height, 20, 32 );
-                    material = new THREE.MeshLambertMaterial( { color: parseInt("0x" + object.color ) } );
-                    break;
-                case "plane":
-                    geometry = new THREE.PlaneGeometry( object.width, object.height );
-                    material = new THREE.MeshBasicMaterial( { color: parseInt("0x" + object.color ) } );
-                    break;
-                case "tetrahedron":
-                    object.radius = parseInt(object.radius || 10);
-                    object.detail = parseFloat(object.detail || 0);
-                    geometry = new THREE.TetrahedronGeometry( object.radius, object.detail );
-                    material = new THREE.MeshLambertMaterial( { color: parseInt("0x" + object.color ) } );
-                    break;
-                case "ring":
-                    object.innerRadius = parseInt( object.innerRadius || 5 );
-                    object.outerRadius = parseInt( object.outerRadius || 10 );
-                    object.smoothness = parseInt( object.smoothness || 8 );
-                    geometry = new THREE.RingGeometry( object.innerRadius, object.outerRadius, object.smoothness );
-                    material = new THREE.MeshLambertMaterial( { color: parseInt( "0x" + object.color ) } );
-                    break;
-                case "torus":
-                    object.radius = parseInt(object.radius || 10);
-                    object.thickness = parseInt(object.thickness || 3);
-                    object.circularSmooth = parseInt(object.circularSmooth || 10);
-                    object.tubeSmooth = parseInt(object.tubeSmooth || 10);
-                    geometry = new THREE.TorusGeometry( object.radius, object.thickness,
-                        object.circularSmooth, object.tubeSmooth);
-                    material = new THREE.MeshLambertMaterial( { color: parseInt( "0x" + object.color ) } );
-                    break;
-                case "torusknot":
-                    object.radius = parseInt(object.radius || 10);
-                    object.tube = parseInt(object.tube || 5);
-                    object.circularSmooth = parseInt(object.circularSmooth || 10);
-                    object.tubeSmooth = parseInt(object.tubeSmooth || 10);
-                    object.p = parseInt(object.p || 2);
-                    object.q = parseInt(object.q || 3);
-                    geometry = new THREE.TorusKnotGeometry( object.radius, object.tube,
-                            object.circularSmooth, object.tubeSmooth, object.p, object.q);
-                    material = new THREE.MeshLambertMaterial( { color: parseInt( "0x" + object.color ) } );
-                    break;
-                case "text":
-                    parameters = {};
-                    object.text = object.text || "OpenVR!";
-                    parameters.size = parseFloat(object.size || 20);
-                    parameters.height = parseFloat(object.height || 50);
-                    parameters.weight = object.weight || "normal";
-                    geometry = new THREE.TextGeometry(object.text, parameters);
-                    break;
-
-            }
-
-            if (object.rotx)
-                geometry.applyMatrix( new THREE.Matrix4().makeRotationX( toRad( parseInt(object.rotx) ) ) );
-            if (object.roty)
-                geometry.applyMatrix( new THREE.Matrix4().makeRotationY( toRad( parseInt(object.roty) ) ) );
-            if (object.rotz)
-                geometry.applyMatrix( new THREE.Matrix4().makeRotationZ( toRad( parseInt(object.rotz) ) ) )
-
-            var mesh = new THREE.Mesh( geometry, material );
-            mesh.position.x = object.position.x;
-            mesh.position.y = object.position.y;
-            mesh.position.z = object.position.z;
-            scene.add( mesh );
-
-            // Check if animation commands exist
-            if ( object.spins || object.orbits ) {
-                if (object.spins)
-                    mesh.spins = object.spins;
-                if (object.orbits)
-                    mesh.orbits = object.orbits;
-                movingObjects.push(mesh);
-            }
-
-            material.color.setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
-
-            objects.push( mesh );
-        });
 
         // var exporter = new THREE.SceneExporter();
         // var sceneJson = JSON.stringify(exporter.parse(scene));
@@ -291,8 +169,17 @@
         // var jScene = JSON.parse(localStorage.getItem('scene'));
         loader.parse(cssImportObject, function( e ) {
             scene = e.scene;
+        console.log( e.scene );
         }, '.');
-        console.log( scene );
+
+
+        // Init camera
+        camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
+
+        // Init controls
+        // controls = new THREE.OculusRiftControls( camera );
+        controls = new THREE.PointerLockControls( camera );
+        scene.add( controls.getObject() );
 
         // Render
 
